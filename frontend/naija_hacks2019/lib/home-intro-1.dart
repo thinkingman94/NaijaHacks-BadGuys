9fe4cfacdfe9b42179e144007c69c52e34ad8815
Zenith Wogwugwu <uzenith360@users.noreply.github.com>
2019-12-13 23:49:43 +0100

    day4-implement

---
+++ frontend/naija_hacks2019/lib/home-intro-1.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'hexcolors.dart';
+
+class HomeIntro1App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+//        appBar: AppBar(
+//          title: Text('Welcome to Flutter'),
+//        ),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/intro-screen-1.jpg"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+//              Text('"Safe Trip"',
+//                textAlign: TextAlign.center,style: TextStyle(
+//                    color: Colors.white,
+//                    fontSize: 40.0
+//                ),),
+              Text('View Real-time Updates Of Your Loved One\'s Whereabouts',
+                textAlign: TextAlign.center,style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.0
+                ),),
+              RaisedButton(
+                color: HexColor.fromHex('#08004C'),
+                textColor: Colors.white,
+                padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
+                child: Text('Next',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      textBaseline: TextBaseline.alphabetic
+                  ),),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/home-intro-2');
+                },
+              )
+            ],
+          )),
+        ),
+      ),
+    );
+  }
+}
